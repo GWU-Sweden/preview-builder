790753798f4e0879895ed2af889f2deb5e3953c0
Tobias Mollstam <tobias@mollstam.com>
2019-10-02 09:20:37 +0200

    properly update status as deploy when deployed, add a comment too

--- jobs.rb
+++ jobs.rb
@@ -15,13 +15,13 @@ Resque.logger.level = Logger::DEBUG
 
 def queue_build(base_repo_name, head_repo_url, head_sha, status_options, pull_request_number)
 	puts "Queueing build for #{head_repo_url}:#{head_sha}"
-	Resque.enqueue(BuildSite, base_repo_name, head_repo_url, head_sha, status_options, pull_request_number)
+	Resque.enqueue(BuildSite, base_repo_name, head_repo_name, head_repo_url, head_sha, status_options, pull_request_number)
 end
 
 class BuildSite
 	@queue = :preview
 
-	def self.perform(base_repo_name, head_repo_url, head_sha, status_options, pull_request_number)
+	def self.perform(base_repo_name, head_repo_name, head_repo_url, head_sha, status_options, pull_request_number)
 		puts "#{head_repo_url}:#{head_sha} -- Starting build..."
 
 		Builder.build(head_repo_url, head_sha) do |error, build_output_tmp|
@@ -38,11 +38,8 @@ class BuildSite
 			puts "built to #{build_output_tmp}\n\t-> #{build_output_storage}"
 			FileUtils.copy_entry(build_output_tmp, build_output_storage)
 
-			status_options = status_options.merge({ :description => "Preview build complete", :target_url => "http://#{pull_request_number}.preview.gwu-sweden.org/?#{head_sha}" })
-			client.create_status(base_repo_name, head_sha, 'success', status_options)
-
 			puts "#{head_repo_url}:#{head_sha} -- Build done"
-			Resque.enqueue(DeploySite, head_repo_url, head_sha, build_output_storage, pull_request_number)
+			Resque.enqueue(DeploySite, base_repo_name, head_repo_url, head_sha, build_output_storage, pull_request_number, status_options)
 		end
 	end
 end
@@ -50,12 +47,27 @@ end
 class DeploySite
 	@queue = :preview
 
-	def self.perform(head_repo_url, head_sha, source, pull_request_number)
-		puts "#{head_repo_url}:#{head_sha} -- Deploying..."
-		deploy_path = File.join(ENV['DEPLOY_DIR'], pull_request_number.to_s)
-		FileUtils.remove_dir(deploy_path, :force => true)
-		puts "Deploying #{source}\n\t-> #{deploy_path}"
-		FileUtils.mv(source, deploy_path)
-		puts "#{head_repo_url}:#{head_sha} -- Deployed to #{deploy_path}"
+	def self.perform(base_repo_name, head_repo_url, head_sha, source, pull_request_number, status_options)
+		client = GitHub::setup_client()
+		begin
+			puts "#{head_repo_url}:#{head_sha} -- Deploying..."
+
+			deploy_path = File.join(ENV['DEPLOY_DIR'], pull_request_number.to_s)
+			FileUtils.remove_dir(deploy_path, :force => true)
+			puts "Deploying #{source}\n\t-> #{deploy_path}"
+			FileUtils.mv(source, deploy_path)
+			puts "#{head_repo_url}:#{head_sha} -- Deployed to #{deploy_path}"
+
+			preview_url = "http://#{pull_request_number}.preview.gwu-sweden.org/?#{head_sha}"
+			client.add_comment(base_repo_name, pull_request_number, ":robot: Preview of changes deployed to #{preview_url}")
+			status_options = status_options.merge({ :description => "Deployed", :target_url => preview_url })
+			client.create_status(base_repo_name, head_sha, 'success', status_options)
+		rescue StandardError => e
+			puts "Deployment failed with error"
+			puts e.message
+			puts e.backtrace.inspect
+			status_options = status_options.merge({ :description => error })
+			client.create_status(base_repo_name, head_sha, 'failure', status_options)
+		end
 	end
 end
